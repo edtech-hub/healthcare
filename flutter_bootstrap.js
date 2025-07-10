e7b79ad86dccb33037067bb31f6a409436274ace
edtech-hub <158812683+edtech-hub@users.noreply.github.com>
2025-07-10 11:56:09 +0530

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "164376803"
+    serviceWorkerVersion: "4236732474"
   }
 });
